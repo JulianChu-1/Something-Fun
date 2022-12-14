eec467714da4f55c238236ef7255e144b7f5d393
JeffreyChu955 <83399707+JeffreyChu955@users.noreply.github.com>
2022-12-14 18:42:23 +0800

    Update DataCleaning.py

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,27 +1,27 @@
 import pandas as pd
 import re
 
-#def dataCleaning(num):
-string1 = r"C:\Users\lenovo\Downloads\cdk ("
-string2 = r").json"
-json_info = ""
-df = pd.DataFrame()
-filename = r"C:\Users\lenovo\Downloads\cdk.json"
-with open(filename,'r', encoding='utf-8') as file:
-    json_info = file.read()
-df_temp = pd.read_json(json_info)
-df = df.append(df_temp,ignore_index = True)
-for i in range(1,20):
-    filename = string1 + str(i) + string2
+def dataCleaning(num):
+    string1 = r"C:\Users\lenovo\Downloads\cdk ("
+    string2 = r").json"
+    json_info = ""
+    df = pd.DataFrame()
+    filename = r"C:\Users\lenovo\Downloads\cdk.json"
     with open(filename,'r', encoding='utf-8') as file:
         json_info = file.read()
     df_temp = pd.read_json(json_info)
     df = df.append(df_temp,ignore_index = True)
-lista = df['content'].to_list()
-patterns = "：(.+)"
-pattern = re.compile(patterns)
-for i in range(len(lista)):
-    lista[i] = pattern.findall(lista[i])
-df['new'] = pd.DataFrame(lista)
-del df['content']
-df.to_excel("data.xlsx")
+    for i in range(1,20):
+        filename = string1 + str(i) + string2
+        with open(filename,'r', encoding='utf-8') as file:
+            json_info = file.read()
+        df_temp = pd.read_json(json_info)
+        df = df.append(df_temp,ignore_index = True)
+    lista = df['content'].to_list()
+    patterns = "：(.+)"
+    pattern = re.compile(patterns)
+    for i in range(len(lista)):
+        lista[i] = pattern.findall(lista[i])
+    df['new'] = pd.DataFrame(lista)
+    del df['content']
+    df.to_excel("data.xlsx")
